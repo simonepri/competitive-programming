fa91692a17050d028f18dfa382451b9152480303
Simone Primarosa <simone.pri@hotmail.it>
2017-02-05 13:50:10 +0100

    Add UVa solution for TEX Quotes (272)

---
+++ UVA/272/272.cpp
@@ -0,0 +1,26 @@
+ /**
+ * Competitive Programming - Collection of solved problems.
+ *
+ * @author        Simone Primarosa,(http://simoneprimarosa.com)
+ * @link          (https://github.com/simonepri/competitive-programming)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  char c;
+  bool state = 0;
+  
+  while(scanf("%c", &c) == 1) {
+    if(c == '"') {
+      if(state) printf("''");
+      else printf("``");
+      state = !state;
+    }
+    else printf("%c", c);
+  }
+	return 0;
+}
