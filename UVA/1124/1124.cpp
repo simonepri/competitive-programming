946f66018415b8b125d72ebc72eb8a04d2b7b4e6
Simone Primarosa <simone.pri@hotmail.it>
2017-02-05 14:37:47 +0100

    Add UVa solution for Celebrity joepardy  (1124)

---
+++ UVA/1124/1124.cpp
@@ -0,0 +1,20 @@
+/**
+* Competitive Programming - Collection of solved problems.
+*
+* @author        Simone Primarosa,(http://simoneprimarosa.com)
+* @link          (https://github.com/simonepri/competitive-programming)
+* @license       MIT License (https://opensource.org/licenses/MIT)
+*/
+
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  char c;
+  
+  while(scanf("%c", &c) == 1) {
+    printf("%c", c);
+  }
+  return 0;
+}
