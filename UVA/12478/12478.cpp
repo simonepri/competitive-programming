ee5b2555d10f893347834bbcc9f80413fcc29b62
simonepri <simone.pri@hotmail.it>
2017-08-24 00:56:56 +0200

    Add UVa solution for Hardest Problem Ever (12478)

---
+++ UVA/12478/12478.cpp
@@ -0,0 +1,19 @@
+/**
+ * Competitive Programming - Collection of solved problems.
+ *
+ * @author        Simone Primarosa,(http://simoneprimarosa.com)
+ * @link          (https://github.com/simonepri/competitive-programming)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false); // faster I/O
+
+  cout << "KABIR" << endl;
+
+  return 0;
+}
