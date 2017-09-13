eb3ce41b40669aa6bc69fca40e4f2f64921d2205
simonepri <simone.pri@hotmail.it>
2017-09-13 10:07:48 +0200

    Improve UVa solution for Clock Hands (579)

--- UVA/579/579.cpp
+++ UVA/579/579.cpp
@@ -18,16 +18,25 @@ int main() {
   ios_base::sync_with_stdio(false); // faster I/O
 
   string line;
+
+  // Print always 3 decimal places.
+  cout << setprecision(3) << fixed;
+
   while (true) {
     cin >> line;
-    double m = stoi(line.substr(line.size() - 2, line.size()));
-    double h = stoi(line.substr(0, line.size() - 3));
+    double m, h;
+    if (line.size() == 5) {
+      h = (line[0] - '0') * 10 + line[1] - '0';
+      m = (line[3] - '0') * 10 + line[4] - '0';
+    } else {
+      h = line[0] - '0';
+      m = (line[2] - '0') * 10 + line[3] - '0';
+    }
     if (m == 0 && h == 0) break;
     if (m > 0) {
       h += m / 60.0;
       if (h > 12) h -= 12;
     }
-
 
     // 360 : 12 = ah : h
     double a1 = 360.0 * h / 12;
@@ -38,8 +47,7 @@ int main() {
     double diff = abs(a1 - a2);
     if (diff > 180) diff = 360.0 - diff;
 
-    // cout << "a1 " << a1 << " a2 " << a2 << endl;
-    cout << setprecision(3) << fixed << diff << endl;
+    cout << diff << endl;
   }
 
   return 0;
