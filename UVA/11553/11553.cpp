6167761d874b44cadc238ce81fbc89f807993268
simonepri <simone.pri@hotmail.it>
2017-10-01 10:48:25 +0200

    Add UVa solution for Grid Game (11553)

--- UVA/11553/11553.cpp
+++ UVA/11553/11553.cpp
@@ -13,21 +13,11 @@
 
 using namespace std;
 
-const int INF_NEG = numeric_limits<int>::min();
-const int INF_POS = numeric_limits<int>::max();
+const int INF = numeric_limits<int>::max();
 
 int N;
-char I[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-char J[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+char COL[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 int M[9][9];
-
-inline int play() {
-  int sum = 0;
-  for (int k = 0; k < N; k++) {
-    sum += M[I[k]][J[k]];
-  }
-  return sum;
-}
 
 int main() {
   ios_base::sync_with_stdio(false); // faster I/O
@@ -44,17 +34,16 @@ int main() {
 
     // when next_permutation return false, it also rearranges the elements
     // according to the first permutation.
-    int max = INF_NEG;
+    int min = INF;
     do {
-      int min = INF_POS;
-      do {
-        int answ = play();
-        if (answ < min) min = answ;
-      } while (next_permutation(J, J + N));
-      if (min > max) max = min;
-    } while (next_permutation(I, I + N));
+      int total = 0;
+      for (int k = 0; k < N; k++) {
+        total += M[k][COL[k]];
+      }
+      if (total < min) min = total;
+    } while (next_permutation(COL, COL + N));
 
-    cout << max << endl;
+    cout << min << endl;
   }
 
   return 0;
